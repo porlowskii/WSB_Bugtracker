9de4cfb735c99a4068d5794b9237cde1423d847a
Pawel Orlowski <p.orlowski@hotmail.com>
2023-02-19 14:24:52 +0100

    initial commit

---
+++ src/main/java/wsb/wsb_bugtracker/WsbBugtrackerApplication.java
@@ -0,0 +1,13 @@
+package wsb.wsb_bugtracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WsbBugtrackerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WsbBugtrackerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/wsb/wsb_bugtracker/WsbBugtrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package wsb.wsb_bugtracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WsbBugtrackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
