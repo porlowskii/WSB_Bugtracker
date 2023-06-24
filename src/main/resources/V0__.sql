29f2af4b4fbf2a58d7c6cc435327baec7e555fc2
Paweł Orłowski <pawel.orlowski@ekoncept.pl>
2023-06-24 11:18:43 +0200

    small fixes

---
+++ src/main/resources/V0__.sql
@@ -0,0 +1,6 @@
+CREATE DATABASE bugtracker;
+
+INSERT INTO authority (id,authority) VALUES
+                                                (1,'ROLE_MANAGE_PROJECT'),
+                                                (2,'ROLE_USER_TAB'),
+                                                (3,'ROLE_MANAGE_USERS');
